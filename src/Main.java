53a06d7b77721b4892bf57e0000ee4a03e781a93
Marie <marie.mathieu@edu.ece.fr>
2025-11-21 11:40:45 +0100

    Enlevage du TEST TEST

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main(String[] args) {
         Menu m = new Menu(); // cree instance de notre classe qui gère l'interface utilisateur
         m.lancer();
-        System.out.println("");
 
     }
 }
